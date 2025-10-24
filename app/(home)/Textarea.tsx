01bea50e11127a66dfd98b4a3656a8c212b494b1
yoonbeeseo <yoon.tec.info@gmail.com>
2025-10-24 17:14:30 +0900

    update

--- app/(home)/Textarea.tsx
+++ app/(home)/Textarea.tsx
@@ -2,27 +2,36 @@
 
 import { twMerge } from "tailwind-merge";
 import { useTextStore } from "./useTextStore";
+import { useState } from "react";
+import { IoEllipsisHorizontal } from "react-icons/io5";
 
 const Textarea = () => {
   const { texts, insertText, isPending, handlePendingState, hasDataReady } =
     useTextStore();
+  const [folded, setFolded] = useState(false);
 
   return (
     <section className="flex flex-col gap-1 m-4">
       <label htmlFor="text_area" className="text-gray-500 text-xs font-bold">
         Your Texts
       </label>
-      <textarea
-        id="text_area"
-        value={texts ?? ""}
-        onChange={(e) => {
-          insertText(e.target.value);
-        }}
-        className={twMerge(
-          "border resize-none rounded p-2 bg-gray-50 border-gray-200 focus:border-blue-500 outline-none focus:bg-transparent min-h-[30vh]",
-          hasDataReady && "min-h-0 max-h-25"
-        )}
-      />
+      {folded ? (
+        <button>
+          <IoEllipsisHorizontal />
+        </button>
+      ) : (
+        <textarea
+          id="text_area"
+          value={texts ?? ""}
+          onChange={(e) => {
+            insertText(e.target.value);
+          }}
+          className={twMerge(
+            "border resize-none rounded p-2 bg-gray-50 border-gray-200 focus:border-blue-500 outline-none focus:bg-transparent min-h-[30vh]",
+            hasDataReady && "min-h-0 max-h-25"
+          )}
+        />
+      )}
       <button
         className="border-0 rounded bg-blue-500 text-white h-12 mt-2 cursor-pointer hover:opacity-80 active:opacity-50 active:scale-99 transition-all disabled:opacity-50"
         disabled={isPending}
--- app/(home)/TransformedData.tsx
+++ app/(home)/TransformedData.tsx
@@ -1,31 +1,85 @@
 "use client";
-import { Fragment, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { TextData, useTextStore } from "./useTextStore";
 import { twMerge } from "tailwind-merge";
+import { IoCheckmark, IoCopy, IoTrashBin } from "react-icons/io5";
 
 const TransformedData = () => {
   const { data } = useTextStore();
   const [selectedText, setSelectedText] = useState<TextData[]>([]);
+  const [copied, setCopied] = useState(false);
+  useEffect(() => {
+    if (copied) {
+      setTimeout(() => setCopied(false), 2000);
+    }
+  }, [copied]);
   return (
     <section>
-      <button
-        className="size-10 rounded border border-gray-200 m-4 float-right cursor-pointer"
-        onClick={() => {
-          const html = data
-            .map(({ index, text }) => {
-              if (selectedText.some((t) => t.index === index)) {
-                return `<u style="text-decoration: underline;">&nbsp;&nbsp;${text}&nbsp;&nbsp;</u>`;
-              }
-              return text;
-            })
-            .join(" ");
+      <div className="flex gap-2 justify-end mx-4 mb-2">
+        <button
+          className="size-10 rounded border border-gray-200 text-gray-500 cursor-pointer flex justify-center items-center"
+          onClick={() => {
+            if (
+              selectedText.length > 0 &&
+              data.length === selectedText.length
+            ) {
+              return setSelectedText([]);
+            }
+            setSelectedText(data);
+          }}
+        >
+          {selectedText.length > 0 && data.length === selectedText.length ? (
+            <IoTrashBin />
+          ) : (
+            "All"
+          )}
+        </button>
+        <button
+          className="size-10 rounded border border-gray-200 cursor-pointer flex justify-center items-center text-gray-500"
+          onClick={() => {
+            const html = data
+              .map(({ index, text }, i) => {
+                const isUnderlined = selectedText.some(
+                  (t) => t.index === index
+                );
+                const next = data[i + 1];
+                const nextIsUnderlined =
+                  next && selectedText.some((t) => t.index === next.index);
 
-          const blob = new Blob([html], { type: "text/html" });
-          navigator.clipboard.write([new ClipboardItem({ "text/html": blob })]);
-        }}
-      >
-        Copy
-      </button>
+                if (isUnderlined) {
+                  return `
+          <span style="margin-right: 4px;">&nbsp;&nbsp;</span>
+          <u style="text-decoration: underline; text-underline-offset: 3px; margin-right: 4px;">
+           &nbsp;&nbsp;${text}&nbsp;&nbsp;
+          </u>
+          ${
+            !nextIsUnderlined
+              ? '<span style="margin-right: 6px;">&nbsp;</span>'
+              : ""
+          }
+        `;
+                }
+
+                return `<span style="margin-right: 6px;">&nbsp;${text}&nbsp;</span>`;
+              })
+              .join("");
+
+            const htmlContent = `
+    <div style="font-size:15px; line-height:160%;">
+      ${html}
+    </div>
+  `;
+            const blob = new Blob([htmlContent], { type: "text/html" });
+            navigator.clipboard.write([
+              new ClipboardItem({ "text/html": blob }),
+            ]);
+            setCopied(true);
+          }}
+        >
+          {copied ? <IoCheckmark /> : <IoCopy />}
+        </button>
+      </div>
+
       <div className="p-4 border mx-4 rounded border-gray-200 bg-gray-50 leading-8">
         {data?.map(({ text, index }) => {
           const selected = selectedText.some(
