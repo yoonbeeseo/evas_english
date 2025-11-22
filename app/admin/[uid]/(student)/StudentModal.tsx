1ee5e86ee47493f651bdd8953148a5e92a8b1edd
Dexter Yoon <dexteryoon@Dexters-iMac.local>
2025-11-22 10:17:45 +0900

    update

--- app/admin/[uid]/(student)/StudentModal.tsx
+++ app/admin/[uid]/(student)/StudentModal.tsx
@@ -1,8 +1,29 @@
-import { useMemo } from "react";
+"use client";
 
-const StudentModal = ({}: ModalPayloadProps<Student>) => {
-  const initialState = useMemo(() => payload ?? {}, [payload]);
-  return <>StudentModal</>;
+import { useForm, useTextInput } from "@/hooks";
+import { useMemo, useState } from "react";
+
+const StudentModal = ({ payload }: ModalPayloadProps<Student>) => {
+  const initialState = useMemo<Student | StudentPayload>(
+    () => payload ?? ({} as StudentPayload),
+    [payload]
+  );
+  const state = useState(initialState);
+
+  const Name = useTextInput();
+  const Dob = useTextInput();
+  const Mobile = useTextInput();
+
+  const { Form, handler, SubmitArea, CancelButton, SubmitButton } = useForm();
+
+  return (
+    <Form>
+      <div className="row">
+        <p>name</p>
+        <p>dob</p>
+      </div>
+    </Form>
+  );
 };
 
 export default StudentModal;
